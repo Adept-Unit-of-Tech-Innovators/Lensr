5c30f6d523dc5afb11deff444a9a7fa84910f6c6
mwadowski <krzyzaczek101@gmail.com>
2023-11-18 16:05:52 +0100

    fix: fixed ray finding intersection when pointed away from the object + fixed ray end point not updating after changing laser start point

--- src/main/java/com/example/lensr/HelloApplication.java
+++ src/main/java/com/example/lensr/HelloApplication.java
@@ -1,8 +1,10 @@
 package com.example.lensr;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.geometry.Point2D;
 import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
@@ -32,6 +34,10 @@ public class HelloApplication extends Application {
         scene.setOnMouseClicked(mouseEvent -> {
             ray[0].setStartX(mouseEvent.getSceneX());
             ray[0].setStartY(mouseEvent.getSceneY());
+
+            // Recalculate ray intersections after it position changed
+            EventHandler<? super MouseEvent> mouseMoved = scene.getOnMouseMoved();
+            mouseMoved.handle(mouseEvent);
         });
 
         scene.setOnMouseMoved(mouseEvent -> {
@@ -45,7 +51,6 @@ public class HelloApplication extends Application {
                 ray[0].setEndX(intersectionPoint.getX());
                 ray[0].setEndY(intersectionPoint.getY());
 
-                // Calculate the angle between the line and the x-axis (angle of incidence)
                 double angleOfIncidence = Math.atan2(ray[0].getEndY() - ray[0].getStartY(), ray[0].getEndX() - ray[0].getStartX());
 
                 // Calculate the angle of the normal vector at the intersection point
@@ -53,7 +58,7 @@ public class HelloApplication extends Application {
 
                 double angleOfReflection = 2 * normalAngle - angleOfIncidence;
 
-                // Calculate the reflected ray second point based on the reflection angle
+                // Calculate the end point of the reflected ray
                 double reflectedX = intersectionPoint.getX() - SIZE * Math.cos(angleOfReflection);
                 double reflectedY = intersectionPoint.getY() - SIZE * Math.sin(angleOfReflection);
 
@@ -61,14 +66,12 @@ public class HelloApplication extends Application {
                 reflectedRay[0].setStartY(intersectionPoint.getY());
                 reflectedRay[0].setEndX(reflectedX);
                 reflectedRay[0].setEndY(reflectedY);
+
                 reflectedRay[0].setStroke(Color.GREEN);
                 reflectedRay[0].setStrokeWidth(2);
+                reflectedRay[0].setVisible(true);
             } else {
-                // Hide the reflected ray if there's no intersection
-                reflectedRay[0].setStartX(0);
-                reflectedRay[0].setStartY(0);
-                reflectedRay[0].setEndX(0);
-                reflectedRay[0].setEndY(0);
+                reflectedRay[0].setVisible(false);
             }
 
             reflectedRay[0].toFront();
@@ -82,13 +85,15 @@ public class HelloApplication extends Application {
         primaryStage.show();
     }
 
-    public static Point2D getCircleIntersectionPoint(Line line, Circle circle) {
-        // Line vector
-        Vector lineVector = new Vector(line.getEndX() - line.getStartX(), line.getEndY() - line.getStartY());
 
+    public static Point2D getCircleIntersectionPoint(Line line, Circle circle) {
+        // If ray is pointing away from the circle, there is no intersection
+        if ( (circle.getCenterX()- line.getStartX()) * line.getEndX() < 0 &&
+             (circle.getCenterY() - line.getStartY()) * line.getEndY() < 0) return null;
+
+        Vector lineVector = new Vector(line.getEndX() - line.getStartX(), line.getEndY() - line.getStartY());
         lineVector = lineVector.normalize();
 
-        // Vector from the start of the line to the circle center
         Vector startToCenterVector = new Vector(circle.getCenterX() - line.getStartX(), circle.getCenterY() - line.getStartY());
 
         double dotProduct = Vector.getDotProduct(lineVector, startToCenterVector);
@@ -100,20 +105,20 @@ public class HelloApplication extends Application {
         // Calculate the distance between the closest point on the line and the circle's center
         double distanceToCenter = Math.sqrt(Math.pow(circle.getCenterX() - closestPointX, 2) + Math.pow(circle.getCenterY() - closestPointY, 2));
 
-        // If the distance is greater than the circle's radius, there is no intersection
         if (distanceToCenter > circle.getRadius()) {
-            return null;
+            return null; // null = no intersection
         }
 
         // Calculate the distance from the closest point on the line to the intersection point
         double distanceToIntersection = Math.sqrt(circle.getRadius() * circle.getRadius() - distanceToCenter * distanceToCenter);
 
-        // Calculate the intersection point
         double intersectionX = closestPointX - distanceToIntersection * lineVector.x;
         double intersectionY = closestPointY - distanceToIntersection * lineVector.y;
 
         return new Point2D(intersectionX, intersectionY);
     }
+
+
     public static double getTangentAngle(Point2D point) {
         double angle = Math.atan2(point.getX(), point.getY());
         angle = Math.toDegrees(angle);
