963c167c25a77f645d2997cffa01091228fb62d0
candour0 <pzdanowiczp@gmail.com>
2023-12-27 15:42:11 +0100

    fix: object edit now closes on mutable value change

--- src/main/java/com/example/lensr/MutableValue.java
+++ src/main/java/com/example/lensr/MutableValue.java
@@ -13,6 +13,14 @@ public class MutableValue {
 
     public void setValue(boolean value) {
         this.value = value;
+        for (Object mirror : LensrStart.mirrors) {
+            if (mirror instanceof LineMirror lineMirror) {
+                lineMirror.closeObjectEdit();
+            }
+            if (mirror instanceof EllipseMirror ellipseMirror) {
+                ellipseMirror.closeObjectEdit();
+            }
+        }
     }
 
     public void setValue(boolean value, MutableValue opositeValue) {
